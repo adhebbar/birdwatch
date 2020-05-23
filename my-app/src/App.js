88f4894e3b29f01ba764cdf1b17a3eeb110d6b13
Aditi Hebbar <aditi@Aditis-MBP.attlocal.net>
2020-05-22 17:27:42 -0700

    basic layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+
+function Map() {
+  return (
+    <Container fluid style= {{"padding":"0px","width": "100%","height": "800px", "background-color": "rgba(255,0,0,0.1)"}}>
+    </Container> 
+  );  
+}
+
+
+function Results() {
+  return (
+    <Container fluid style= {{"padding":"0px","width": "100%","height": "800px", "background-color": "rgba(0,255,0,0.1)"}}>
+    </Container> 
+  );  
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Container fluid>
+        <Row>
+          <Col xs="9" ><Map/> </Col>
+          <Col xs="3" ><Results/></Col>
+        </Row>
+      </Container>
+    );  
 }
 
 export default App;
