adc65d2d5c327866fb7db4b1a579b6e8f90d7313
Aditi Hebbar <aditi@Aditis-MBP.attlocal.net>
2020-05-23 16:35:38 -0700

    event listner for center change working

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from 'reactstrap';
 import React, { Component } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript,useGoogleMap,Marker } from '@react-google-maps/api';
 
 
 const containerStyle = {
@@ -13,24 +13,78 @@ const center = {
   lng: -38.523
 };
 
-class MyComponents extends Component {
-  render() {
+// function PanningComponent() {
+//   const map = useGoogleMap()
+//   console.log()
+//   return null
+// }
+
+// function NewMap(){
+
+//   let mapRef = React.createRef()
+
+//   boundsCallBack = () => {
+//       console.log("foo")
+//       // console.log(mapRef.current.getInstance())
+
+//   }
+
+//   return (
+//       <LoadScript googleMapsApiKey={"AIzaSyDwRk43Y4b1iJkQ1x-TIRCozqnMUyydC9Q"}>
+//           <GoogleMap
+//             mapContainerStyle={containerStyle}
+//             center={center}
+//             zoom={3}
+//             ref={mapRef}
+//             onCenterChanged={(foo) => boundsCallBack}
+//           >
+            
+//           </GoogleMap>
+//       </LoadScript>
+//   )
+// }
+
+function RecenterComponent(){
+  const map = useGoogleMap()
+  console.log("yes")
+  React.useEffect(() => {
+    if (map) {
+      map.addListener('center_changed', function() {
+        console.log(map.getCenter()) // zoom_changed
+        console.log(map.getCenter().lat())
+        console.log(map.getCenter().lng())
+      });
+    }
+  },[map])
+
+  return null
+}
+
+function MyComponents(){
+  
+  function boundsCallBack()  {
+    }
+
     return (
       <LoadScript
         googleMapsApiKey="AIzaSyDwRk43Y4b1iJkQ1x-TIRCozqnMUyydC9Q"
       >
+        
         <GoogleMap
+
           mapContainerStyle={containerStyle}
           center={center}
           zoom={3}
+          onBoundsChanged = {() => console.log("bounds changed")}
+          onCenterChanged = {() => RecenterComponent}
         >
+          <RecenterComponent/>
           { /* Child components, such as markers, info windows, etc. */ }
-          <></>
+          <></> */}
         </GoogleMap>
       </LoadScript>
     )
   }
-}
 
 
 function Map() {
