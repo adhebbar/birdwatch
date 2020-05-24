b36671212af3452a5c7ff6ca3da400a04181ca45
Aditi Hebbar <aditi@Aditis-MBP.attlocal.net>
2020-05-24 00:51:04 -0700

    marker circle broken

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from 'reactstrap';
 import React, { Component } from 'react';
-import { GoogleMap, LoadScript,useGoogleMap,Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript,useGoogleMap,Marker ,Circle} from '@react-google-maps/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCenter , setZoom, fetchBirds} from '../redux/actions'
 
@@ -53,7 +53,34 @@ function MyComponents(){
   if (cards) {
     console.log(cards[0])
   }
-  console.log(cards)
+  const onLoad = marker => {
+    console.log('marker: ', marker)
+  }
+
+  const center = useSelector(state => state.center)
+
+  const options = {
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 30000,
+    zIndex: 1
+  }
+
+  const onLoadc = circle => {
+    console.log('Circle onLoad circle: ', circle)
+  }
+  
+  const onUnmount = circle => {
+    console.log('Circle onUnmount circle: ', circle)
+  }
+  
     return (
       <LoadScript
         googleMapsApiKey="AIzaSyDwRk43Y4b1iJkQ1x-TIRCozqnMUyydC9Q"
@@ -66,8 +93,22 @@ function MyComponents(){
           zoom={3}
         >
           <RecenterComponent/>
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></> */}
+          <Marker
+          onLoad={onLoad}
+          position={{lat:center.lat,lng:center.lng}}
+        />
+
+        <Circle
+              // optional
+              onLoad={onLoadc}
+              // optional
+              onUnmount={onUnmount}
+              // required
+              center={{lat:center.lat,lng:center.lng}}
+              // required
+              options={options}
+            />
+
         </GoogleMap>
       </LoadScript>
     )
